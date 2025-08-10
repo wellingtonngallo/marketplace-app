75be57d4583217ef789341b0f2d6f6e06100e658
wellingtonngallo <wellinton.gallo77@gmail.com>
2025-08-10 20:59:54 -0300

    fix: config tailwind css

--- marketplace-host/vite.config.ts
+++ marketplace-host/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwind from '@tailwindcss/vite'
 import federation from '@originjs/vite-plugin-federation'
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwind(),
     federation({
       remotes: {
         header: `http://localhost:5001/assets/remoteEntry.js`,
